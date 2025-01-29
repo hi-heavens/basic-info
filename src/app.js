f96bee58a763c0f27aed148f2400bdef5928ef08
Olaitan Kehinde <ken_adex@yahoo.com>
2025-01-29 08:05:54 +0100

    feat: added CORS in the app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+const cors = require("cors");
 const express = require("express");
 const personRoute = require("./person/person.route");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", personRoute);
