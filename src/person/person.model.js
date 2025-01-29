018796282898badf078e7119d4d471b2d5f501f9
Olaitan Kehinde <ken_adex@yahoo.com>
2025-01-29 12:21:38 +0100

    updated the response fields

--- src/person/person.model.js
+++ src/person/person.model.js
@@ -2,8 +2,8 @@ const currentDateTime = new Date().toISOString();
 
 const person = {
   email: "ken_adex@yahoo.com",
-  currentDateTime,
-  githubURL: "https://github.com/hi-heavens/basic-info",
+  current_datetime: currentDateTime,
+  github_url: "https://github.com/hi-heavens/basic-info",
 };
 
 module.exports = person;
